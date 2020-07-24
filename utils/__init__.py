73ed3fc4208438aee98b9f8e7b2c43ad303183d7
bolivierjr <bolivierjr@gmail.com>
2020-07-23 20:52:26 -0500

    Added reload() for the users util module so the reload command will reload it with the rest of the modules.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,9 @@
 from importlib import reload
 
-from . import errors, services, weather
+from . import errors, services, users, weather
 
 # To reload the modules when you reload the bot.
 reload(errors)
 reload(weather)
 reload(services)
+reload(users)
